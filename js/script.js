82ce52ef50e57b175f518cbd8509cff89d53799b
Victor Wesley <vic.devjs@gmail.com>
2024-12-22 02:07:23 -0300

    v1.6 (criação do novo projeto)

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// let buttons = {
+//     newbie : document.querySelector("#newbieButton").classList,
+//     junior : document.querySelector("#juniorButton").classList,
+//     intermediated : document.querySelector("#intermediateButton").classList,
+//     advanced : document.querySelector("#advancedButton").classList
+// }
+
+// function toggleCategory(button){
+//     if(button == "disabled"){
+//         button.add("actived")
+//     }
+// };
+
+// function toggleNewbie(){
+    
+// };
+
+
+
+
+// // if (newbie == "disabled") {
+//     //     newbie = "actived";
+//     //     junior = "disabled";
+//     //     intermediated = "disabled";
+//     //     advanced = "disabled";
+//     // }else 
+//     // if (buttons.junior == "disabled" || buttons.junior == "removed") {
+//     //     buttons.newbie.remove("actived");
+//     //     buttons.newbie.add("remove")
+
+//     //     buttons.junior.remove("removed");
+//     //     buttons.junior.add("actived");
+
+//     //     buttons.intermediated.remove("actived");
+//     //     buttons.intermediated.add("removed");
+
+//     //     buttons.advanced.remove("actived");
+//     //     buttons.advanced.add("removed");
+//     // }
+//     // if (buttons.intermediated == "disabled" || buttons.intermediated == "removed") {
+//     //     buttons.newbie.remove("actived");
+//     //     buttons.newbie.add("remove")
+
+//     //     buttons.junior.remove("actived");
+//     //     buttons.junior.add("removed");
+
+//     //     buttons.intermediated.remove("removed");
+//     //     buttons.intermediated.add("actived");
+
+//     //     buttons.advanced.remove("actived");
+//     //     buttons.advanced.add("removed");
+//     //     console.log(buttons.newbie)
+//     //     console.log(buttons.junior)
+//     //     console.log(buttons.intermediated)
+//     //     console.log(buttons.advanced)
+//     // }
+
+
+
+//     // }else if (intermediated == "disabled") {
+//     //     newbie = "disabled";
+//     //     junior = "disabled";
+//     //     intermediated = "actived";
+//     //     advanced = "disabled";
+//     // }else if (advanced == "disabled") {
+//     //     newbie = "disabled";
+//     //     junior = "disabled";
+//     //     intermediated = "disabled";
+//     //     advanced = "actived";
+//     // }else{
+//     //     console.log("Erro!")
+//     // }
